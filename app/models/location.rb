b9e2e8c03142a029ad7d8f1a4bd7b41452e2c303
Timon Vonk <mail@timonv.nl>
2011-06-25 15:21:23 +0200

    Made location.to_s nice

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,7 @@
 class Location < ActiveRecord::Base
   belongs_to :event
+  
+  def to_s
+    "#{address} - #{town}, #{country}"
+  end
 end
--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -2,4 +2,11 @@ require 'spec_helper'
 
 describe Location do
   it { should belong_to(:event) }
+
+  describe "methods" do
+    describe "to_s" do
+      location = Fabricate(:location)
+      location.to_s.should == ("#{location.address} - #{location.town}, #{location.country}")
+    end
+  end
 end
