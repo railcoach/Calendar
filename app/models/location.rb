3c7e3e5ed96a15cbc463514e6ee208ca000cdac2
Timon Vonk <mail@timonv.nl>
2011-06-25 15:44:16 +0200

    updated for town, country only

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,7 +1,14 @@
 class Location < ActiveRecord::Base
   belongs_to :event
+  validates :country
   
   def to_s
-    "#{address} - #{town}, #{country}"
+    if address && town && country
+      "#{address} - #{town}, #{country}"
+    elsif town && country
+      "#{town}, #{country}"
+    elsif country
+      "#{country}"
+    end
   end
 end
