334d3e1994cd55db3a4fc9c66c5ac87f1035e57f
Timon Vonk <mail@timonv.nl>
2011-07-02 12:55:52 +0200

    Checkbox allday

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110626125732) do
+ActiveRecord::Schema.define(:version => 20110701131804) do
 
   create_table "events", :force => true do |t|
     t.datetime "starts_at"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(:version => 20110626125732) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+    t.boolean  "all_day"
   end
 
   create_table "locations", :force => true do |t|
